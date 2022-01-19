f36b1d27013e70c1d2567ceb9802e324e5781ba1
JadKhater123 <98041597+JadKhater123@users.noreply.github.com>
2022-01-19 23:51:28 +0200

    trying again

--- tranquilloAPI/settings.py
+++ tranquilloAPI/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "!tq$+(+9jspz&(i&=ekmqo=d_6fv7qdigl*^c2agn64y9um$_h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', '127.0.0.1']
 
